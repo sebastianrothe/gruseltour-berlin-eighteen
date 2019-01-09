c55ffe94a420a874c802a40b57de8ae95e3fece1
Sebastian Rothe <rothe@infai.org>
2019-01-09 22:33:35 +0100

    add custom functions file replace jquery and jquery-migrate with latest versions

--- functions.php
+++ functions.php
@@ -615,3 +615,8 @@ require get_parent_theme_file_path( '/inc/icon-functions.php' );
  * WooCommerce adjustments functions and filters.
  */
  require get_parent_theme_file_path( '/inc/woocommerce-functions.php' );
+
+ /**
+ * Gruseltour Berlin adjustments functions and filters.
+ */
+require get_parent_theme_file_path( '/inc/gtberlin-functions.php' );
