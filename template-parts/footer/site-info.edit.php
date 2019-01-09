1baac74202c6c92cbe17905cd2a799fe2913ae90
Sebastian Rothe <rothe@infai.org>
2019-01-09 22:24:34 +0100

    add edited file

---
+++ template-parts/footer/site-info.edit.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<?php
+if (function_exists('the_privacy_policy_link')) {
+    the_privacy_policy_link('', '<span role="separator" aria-hidden="true"></span>');
+}
+?>
+	<a href="/impressum/" class="imprint">Impressum</a>
+	<span role="separator" aria-hidden="true"></span>
+	<a href="/jobs/" class="imprint">Jobs</a>
+	<span role="separator" aria-hidden="true"></span>
+	<span>Gruseltour Berlin 👻 is made with ❤️ and ☕ in Berlin and Leipzig. © 2018</span>
+</div><!-- .site-info -->
