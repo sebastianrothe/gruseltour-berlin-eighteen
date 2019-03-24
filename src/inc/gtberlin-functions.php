bb04993a53c88ed1c5069aab6efea7dd1325cb98
Sebastian Rothe <rothe@infai.org>
2019-03-24 12:12:19 +0100

    remove migrate from list of scripts

--- src/inc/gtberlin-functions.php
+++ src/inc/gtberlin-functions.php
@@ -13,3 +13,8 @@ function update_jquery(){
     wp_register_script('jquery', ("https://code.jquery.com/jquery-3.3.1.min.js"), false, '3.3.1', true);
 	wp_enqueue_script('jquery');
 }
+
+add_action('wp_enqueue_scripts', 'remove_jquery_migrate');
+function remove_jquery_migrate(){
+    wp_deregister_script('jquery-migrate');
+}
