5356f79afa5a0b1c47be30c240c8c10658eab44c
Sebastian Rothe <rothe@infai.org>
2019-05-05 12:46:57 +0200

    update jquery to 3.4.1

--- src/inc/gtberlin-functions.php
+++ src/inc/gtberlin-functions.php
@@ -10,7 +10,7 @@
 add_action('wp_enqueue_scripts', 'update_jquery');
 function update_jquery(){
     wp_deregister_script('jquery');
-    wp_register_script('jquery', ("https://code.jquery.com/jquery-3.3.1.min.js"), false, '3.3.1', true);
+    wp_register_script('jquery', ("https://code.jquery.com/jquery-3.4.1.min.js"), false, '3.4.1', true);
 	wp_enqueue_script('jquery');
 }
 
