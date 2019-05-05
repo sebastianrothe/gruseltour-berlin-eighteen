3da7d5f15b0bbd987a8f749cf019486563dad72b
Sebastian Rothe <rothe@infai.org>
2019-05-05 12:56:54 +0200

    use version from param

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-zip -r -9 dist/gruseltour-berlin-eighteen-1.0.0.zip src \
+VERSION=${1:-develop}
+
+zip -r -9 dist/gruseltour-berlin-eighteen-$VERSION.zip src \
     -x */.DS_Store \
     -x */.git \
     -x */.svn \
