ea9ac86be959125f39f5dd60eafec24ce01d938d
Sebastian Rothe <rothe@infai.org>
2020-04-08 23:34:41 +0200

    delete all customizations to parent

--- parent/template-parts/footer/site-info.php
+++ parent/template-parts/footer/site-info.php
@@ -11,11 +11,11 @@
 ?>
 <div class="site-info">
 	<?php
-if (function_exists('the_privacy_policy_link')) {
-    the_privacy_policy_link('', '<span role="separator" aria-hidden="true"></span>');
-}
-?>
-	<a href="/impressum/" class="imprint">Impressum</a>
-	<span role="separator" aria-hidden="true"></span>
-	<span>Gruseltour Berlin 👻 is made with ❤️ and ☕ in Berlin and Leipzig. © <?php echo date("Y"); ?></span>
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	}
+	?>
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" class="imprint">
+		<?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?>
+	</a>
 </div><!-- .site-info -->
